2e332279c83996da4fdc4f0c8c5ce3627eaa6521
Alexander <Alex3945258@yandex.ru>
2021-06-20 17:58:59 +0300

    refactor replace svg in arrow component

--- src/components/Carousel/Arrow/Arrow.jsx
+++ src/components/Carousel/Arrow/Arrow.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import nextArrow from '../../../assets/arrow_forward_ios_black_24dp.svg';
+import prevArrow from '../../../assets/arrow_back_ios_black_24dp.svg';
 
 
 const ArrowStyled = styled.div`
@@ -26,11 +28,7 @@ const Arrow = (props) => {
 
     return (
       <ArrowStyled direction={props.direction} onClick={props.handleClick}>
-        {props.direction === 'right'  
-          ? <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="20px" viewBox="0 0 24 24" width="20px" fill="gray">
-            <g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><polygon points="6.23,20.23 8,22 18,12 8,2 6.23,3.77 14.46,12"/></g></svg> 
-          : <svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="gray">
-            <path d="M0 0h24v24H0V0z" fill="none" opacity=".87"/><path d="M17.51 3.87L15.73 2.1 5.84 12l9.9 9.9 1.77-1.77L9.38 12l8.13-8.13z"/></svg>}
+        {props.direction === 'right' ? <img src={nextArrow} alt='' /> : <img src={prevArrow} alt='' />}
       </ArrowStyled>
     )
   };
